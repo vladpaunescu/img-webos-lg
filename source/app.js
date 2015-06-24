dc36dd75edc50a8534ca767ba262ea79ed685aa2
Vlad Păunescu <vlad.paunescu@gmail.com>
2015-06-24 18:52:10 +0300

    Some experiments

--- source/app.js
+++ source/app.js
@@ -5,11 +5,107 @@
 */
 
 enyo.kind({
-	name: "flickr.Application",
+	name: "imgrec.Application",
 	kind: "enyo.Application",
 	view: "flickr.MainView"
 });
 
-enyo.ready(function () {
-	new flickr.Application({name: "app"});
+// enyo.ready(function () {
+// 	new imgrec.Application({name: "app"});
+// });
+
+
+enyo.ready(function() {
+    enyo.AjaxSource.create({ name: 'ajax' });
+    var collection = new enyo.Collection({
+        source: 'ajax',
+        url: 'http://lg1.arnia.ro/api/axes/',
+        options: {parse: true},
+
+        parse: function(data) {
+     		return data && data.attributes_reply.complete;
+     	}
+    });
+
+    // enyo.kind({
+    //     name: 'RepoView',
+    //     kind: 'DataRepeater',
+    //     collection: collection,
+    //     components: [
+    //     	{	kind: "moon.ExpandableListItem", name: "header",
+    //     		components: [{content: "Item One"}],
+    //     		bindings: [
+    //             	{ from: 'model.name', to: '$.header.content' }
+    //                  ///  from: 'model.classes', to: '$.axes'
+    //             ]
+    //     	}]
+    // });
+
+    enyo.kind({
+        name: 'AxeView',
+        kind: 'DataRepeater',
+        components: [{
+        	components: [{ name: 'axe' },       
+        				],
+        	bindings: [
+                { from: 'model.name', to: '$.axe.content' },
+           ]
+        }]
+    });
+
+
+    enyo.kind({
+        name: 'RepoView',
+        kind: 'DataList',
+        collection: collection,
+
+		components: [
+			{kind: "moon.DataGridList", fit:true, name: "resultList", minWidth: 250, minHeight: 300, ontap: "itemSelected", components: [
+				{kind: "moon.GridListImageItem", imageSizing: "cover", useSubCaption:false, centered:false, bindings: [
+					{from: "model.title", to:"caption"},
+					{from: "model.thumbnail", to:"source"}
+				]}
+			]}
+		],
+
+        // components: [{
+        // 	components: [{ name: 'name' },
+        // 			//      new AxeView({'name': 'axes'})     
+        // 				],
+        // 	bindings: [
+        //         { from: 'model.name', to: '$.name.content' },
+        //          {from: 'model.classes', to: '$.axes.collection'}
+        //    ]
+        // }]
+    });
+
+// enyo.ready(function() {
+//     enyo.AjaxSource.create({ name: 'ajax' });
+//     var collection = new enyo.Collection({
+//         source: 'ajax',
+//         url: 'https://api.github.com/users/enyojs/repos'
+//     });
+
+//     enyo.kind({
+//         name: 'RepoView',
+//         kind: 'DataRepeater',
+//         collection: collection,
+//         components: [{
+//             components: [{ name: 'name' }],
+//             bindings: [
+//                 { from: 'model."full_name"', to: '$.name.content' }
+//             ]
+//         }]
+//     });
+
+//     new enyo.Application({ name: 'app', view: 'RepoView' });
+
+//     collection.fetch();
+// });
+
+
+
+    new enyo.Application({ name: 'app', view: 'RepoView' });
+
+    collection.fetch();
 });
--- source/data/data.js
+++ source/data/data.js
@@ -4,79 +4,65 @@
 	these kind definitions into multiple files under this folder.
 */
 
+// enyo.kind({
+// 	name: "flickr.Source",
+// 	kind: "enyo.JsonpSource",
+// 	urlRoot: "https://api.flickr.com/services/rest/",
+// 	fetch: function(rec, opts) {
+// 		opts.callbackName = "jsoncallback";
+// 		opts.params = enyo.clone(rec.params);
+// 		opts.params.api_key = "2a21b46e58d207e4888e1ece0cb149a5";
+// 		opts.params.format = "json";
+// 		this.inherited(arguments);
+// 	}
+// });
+
 enyo.kind({
-	name: "flickr.Source",
-	kind: "enyo.JsonpSource",
-	urlRoot: "https://api.flickr.com/services/rest/",
+	name: "imgrec.AxesSource",
+	kind: "enyo.AjaxSource",
+	urlRoot: "http://lg1.arnia.ro/api/axes/",
 	fetch: function(rec, opts) {
-		opts.callbackName = "jsoncallback";
 		opts.params = enyo.clone(rec.params);
-		opts.params.api_key = "2a21b46e58d207e4888e1ece0cb149a5";
 		opts.params.format = "json";
 		this.inherited(arguments);
 	}
 });
 
-new flickr.Source({name: "flickr"});
+enyo.AjaxSource.create({name: 'imgrec.AxesSource'});
 
 enyo.kind({
-	name: "flickr.ImageModel",
-	kind: "enyo.Model",
-	options: { parse: true },
-	source: "flickr",
-	computed: [
-		{method: "thumbnail", path: ["farm", "server", "id", "secret"]},
-		{method: "original", path: ["farm", "server", "id", "secret"]}
-	],
-	thumbnail: function() {
-		return "https://farm" + this.get("farm") +
-			".static.flickr.com/" + this.get("server") +
-			"/" + this.get("id") + "_" + this.get("secret") + "_m.jpg";
-	},
-	original: function() {
-		return "https://farm" + this.get("farm") +
-			".static.flickr.com/" + this.get("server") +
-			"/" + this.get("id") + "_" + this.get("secret") + ".jpg";
-	},
+    name: "imgrec.AxesModel",
+    kind: "enyo.Model",
+    source: "imgrec.AxesSource",
+
 	fetch: function(opts) {
 		this.params = {
-			method: "flickr.photos.getinfo",
-			photo_id: this.get("id")
+			
 		};
 		return this.inherited(arguments);
 	},
-	parse: function(data) {
-		data = data.photo || data;
-		data.title = data.title._content || data.title;
-		data.username = data.owner && data.owner.realname;
-		data.taken = data.dates && data.dates.taken;
-		return data;
-	}
+    parse: function(data) {
+     	console.log("Data is ", data.result);
+        return data.result;
+     }
 });
 
-enyo.kind({
-	name: "flickr.SearchCollection",
-	kind: "enyo.Collection",
-	model: "flickr.ImageModel",
-	source: "flickr",
-	options: { parse: true },
-	published: {
-		searchText: null
-	},
-	searchTextChanged: function() {
-		this.empty({destroy: true});
-		this.fetch();
-	},
-	fetch: function(opts) {
-		this.params = {
-			method: "flickr.photos.search",
-			sort: "interestingness-desc",
-			per_page: 50,
-			text: this.searchText
-		};
-		return this.inherited(arguments);
-	},
-	parse: function(data) {
-		return data && data.photos && data.photos.photo;
-	}
-});
+ var myModel = new imgrec.AxesModel();
+ console.log(myModel.fetch());
+
+// new imgrec.Source({name: "imgrec"});
+
+// enyo.kind({
+// 	name: "imgrec.SearchCollection",
+// 	kind: "enyo.Collection",
+// 	source: "imgrec",
+// 	create: function() {
+// 		this.fetch();
+// 	},
+// 	fetch: function(opts) {
+// 		this.params = {};
+// 		resp = this.inherited(arguments);
+// 		console.log("here", resp);
+// 		return resp;
+// 	}
+// });
