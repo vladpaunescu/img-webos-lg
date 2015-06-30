e5af0d919a51daf2f595700ad84b8fb0416e9284
Vlad Păunescu <vlad.paunescu@gmail.com>
2015-06-30 18:02:29 +0300

    Added some code

--- source/app.js
+++ source/app.js
@@ -16,16 +16,113 @@ enyo.kind({
 
 
 enyo.ready(function() {
+
     enyo.AjaxSource.create({ name: 'ajax' });
     var collection = new enyo.Collection({
         source: 'ajax',
-        url: 'http://lg1.arnia.ro/api/axes/',
+        url: 'http://localhost:8081/api/axes/',
         options: {parse: true},
 
         parse: function(data) {
+            console.log(data.attributes_reply.complete);
      		return data && data.attributes_reply.complete;
      	}
     });
+
+
+    enyo.kind({
+        name: "CategoryCollection",
+        kind: "enyo.collection",
+        options: {parse: true},
+        parse: function(data) {
+            return data && data.attributes_reply.complete;
+        }
+    });
+
+    enyo.kind ({
+        name: 'ClassItemView',
+        model: null,
+        components: [
+             {kind: "moon.Item", name: "header"},
+        ],
+        bindings: [
+            { from: '.model.name', to: '$.header.content' }
+        ],
+    });
+
+    enyo.kind({
+        name: 'CategoryDataList',
+        kind: 'DataRepeater',
+        collection: null,
+        components: [{            
+           components: [
+             {kind: "moon.Item", name: "header"},
+            ],
+            bindings: [
+            { from: '.model.name', to: '.$.header.content' }
+            ]
+        }]
+    });
+
+    enyo.kind ({
+        name: 'CategoryView',
+        model: null,
+        components: [
+            {kind: 
+                "moon.ExpandableListItem", 
+                name: "header",  
+                content: null,
+                published: {
+                    data: null
+                },
+                components:  [
+                { kind: 'enyo.Repeater',
+                    name: "repeater",
+                    onSetupItem: "setUpItem",
+                    components: [
+                        {kind: "moon.Item"}
+                    ]
+                }
+                ],
+                setUpItem: function(inSender, inEvent) {
+                     var index = inEvent.index;
+                     var item = inEvent.item;
+                     inEvent.item.$.setContent(this.collection[index].name);
+                     console.log(this.collection);
+                },
+                dataChanged: function() {
+                   console.log(this.data.length);
+                   this.$.repeater.setCount(this.data.length);
+                   return true;
+                },
+                create: function() {
+                    this.inherited(arguments);
+                    this.$.repeater.setCount(this.data.length);
+                },
+            }
+              
+        ],
+        bindings: [
+            { from: 'model.name', to: '$.header.content' },
+            { from: 'model.classes', to: '.$.header.data' }
+        ]
+    });
+
+    enyo.kind({
+        name: 'AxesDataList',
+        kind: 'DataList',
+        collection: collection,
+        components: [
+            {kind: 'CategoryView'}
+        ]
+    });
+
+
+    new enyo.Application({ name: 'app', view: 'AxesDataList' });
+
+    collection.fetch();
+
+
 
     // enyo.kind({
     //     name: 'RepoView',
@@ -53,35 +150,35 @@ enyo.ready(function() {
     //     }]
     // });
 
-   enyo.kind({
-        name: 'AxeView',
-        kind: 'DataList',
-        collection: null,
-		components: [
-			{kind: "moon.Item", bindings: [
-					{from: "model", to:"content"}
-				]
-			}
-			]
-    });
+  //  enyo.kind({
+  //       name: 'AxeView',
+  //       kind: 'DataList',
+  //       collection: null,
+		// components: [
+		// 	{kind: "moon.Item", bindings: [
+		// 			{from: "model", to:"content"}
+		// 		]
+		// 	}
+		// 	]
+  //   });
 
-    enyo.kind({
-        name: 'RepoView',
-        kind: 'DataList',
-        collection: collection,
-		components: [
-			// {kind: "moon.Item", bindings: [
-			// 		{from: "model.name", to:"content"}
-			// 	]
-			// },
-			{
-				kind :"AxeView",
-				bindings: [
-				{from: "model.classes", to: "collection"}
-			]
-			}
-			]
-    });
+  //   enyo.kind({
+  //       name: 'RepoView',
+  //       kind: 'DataList',
+  //       collection: collection,
+		// components: [
+		// 	// {kind: "moon.Item", bindings: [
+		// 	// 		{from: "model.name", to:"content"}
+		// 	// 	]
+		// 	// },
+		// 	{
+		// 		kind :"AxeView",
+		// 		bindings: [
+		// 		{from: "model.classes", to: "collection"}
+		// 	]
+		// 	}
+		// 	]
+  //   });
 
 // enyo.ready(function() {
 //     enyo.AjaxSource.create({ name: 'ajax' });
@@ -109,7 +206,4 @@ enyo.ready(function() {
 
 
 
-    new enyo.Application({ name: 'app', view: 'RepoView' });
-
-    collection.fetch();
 });
